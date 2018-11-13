d839d0a3a975a36fb88b12f706e5563d5d46152f
ziyuan <hanziyuan08@outlook.com>
2018-11-13 18:00:13 +0800

    gateway

--- user/src/main/java/com/github/bpazy/home/user/UserController.java
+++ user/src/main/java/com/github/bpazy/home/user/UserController.java
@@ -1,6 +1,5 @@
 package com.github.bpazy.home.user;
 
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @SneakyThrows
     @RequestMapping("/findUser/{userId}")
     public User findUser(@PathVariable String userId) {
         return userService.findUser(userId);
