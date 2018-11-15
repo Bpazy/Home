0d92b287b0349f47c8ba666724397098b59a6b18
ziyuan <hanziyuan08@outlook.com>
2018-11-15 17:59:37 +0800

    security

---
+++ security/src/main/java/com/github/bpazy/home/security/SecurityApplication.java
@@ -0,0 +1,12 @@
+package com.github.bpazy.home.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
