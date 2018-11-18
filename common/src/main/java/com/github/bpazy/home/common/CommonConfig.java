781c62a9a07e6bd6c90cedc859adfb1d981bbe46
ziyuan <onbpazy@163.com>
2018-11-19 00:04:06 +0800

    remove unnecessary response handle

--- common/src/main/java/com/github/bpazy/home/common/CommonConfig.java
+++ common/src/main/java/com/github/bpazy/home/common/CommonConfig.java
@@ -1,25 +1,11 @@
 package com.github.bpazy.home.common;
 
-import com.github.bpazy.home.common.spring.PermissionExceptionInterceptor;
-import feign.codec.Decoder;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configurable
 @ComponentScan
 public class CommonConfig implements WebMvcConfigurer {
-    @Autowired
-    private ObjectFactory<HttpMessageConverters> messageConverters;
 
-    @Bean
-    public Decoder decoder() {
-        return new PermissionExceptionInterceptor(new ResponseEntityDecoder(new SpringDecoder(messageConverters)));
-    }
 }
